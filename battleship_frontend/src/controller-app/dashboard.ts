1003abef1d3b218bba446b82d6f57253083b599b
Anthony Mahanna <anthony.mahanna@gmail.com>
2020-04-26 14:12:05 -0400

    lesson5 changes

--- battleship_frontend/src/controller-app/dashboard.ts
+++ battleship_frontend/src/controller-app/dashboard.ts
@@ -1,7 +1,12 @@
 import { EventAggregator } from "aurelia-event-aggregator";
 import { Router } from "aurelia-router";
 import { GameParams } from "common/game-params";
-import { MoveResponseEvent, TopicHelper, GameStart, InternalMoveResult } from "../common/events";
+import {
+  MoveResponseEvent,
+  TopicHelper,
+  GameStart,
+  InternalMoveResult,
+} from "../common/events";
 import { bindable, inject } from "aurelia-framework";
 import { SolaceClient } from "common/solace-client";
 
@@ -16,19 +21,42 @@ class MoveResultMap {
   Player2: MoveResponseEvent;
 }
 
-@inject(SolaceClient, TopicHelper, GameParams, Router, GameStart, EventAggregator)
+@inject(
+  SolaceClient,
+  TopicHelper,
+  GameParams,
+  Router,
+  GameStart,
+  EventAggregator
+)
 export class Dashboard {
   private action: string;
   private moveResultMap: MoveResultMap = new MoveResultMap();
   private scoreMap: ScoreMap = new ScoreMap();
   private turnMessage: string;
 
-  constructor(private solaceClient: SolaceClient, private topicHelper: TopicHelper, private gameParams: GameParams, private router: Router, private gameStart: GameStart, private ea: EventAggregator) {
+  constructor(
+    private solaceClient: SolaceClient,
+    private topicHelper: TopicHelper,
+    private gameParams: GameParams,
+    private router: Router,
+    private gameStart: GameStart,
+    private ea: EventAggregator
+  ) {
     this.scoreMap.Player1 = gameParams.allowedShips;
     this.scoreMap.Player2 = gameParams.allowedShips;
     this.turnMessage = "Player1's Turn";
 
     //Subscribe to all MOVE-REPLYs from Player1 and Player2 to propogate in the dashboard
+    this.solaceClient.subscribe(
+      `${this.topicHelper.prefix}/MOVE-REPLY/*/*`,
+      (msg) => {
+        let moveResponseEvent: MoveResponseEvent = JSON.parse(
+          msg.getBinaryAttachment()
+        );
+        this.moveResultMap[moveResponseEvent.player] = moveResponseEvent;
+      }
+    );
   }
 
   attached() {
@@ -38,7 +66,9 @@ export class Dashboard {
         this.action = "hit";
         this.scoreMap[imr.player] -= 1;
         if (this.scoreMap[imr.player] == 0) {
-          this.router.navigateToRoute("game-over", { msg: `${imr.player == "Player1" ? "Player2" : "Player1"} WINS!!!!` });
+          this.router.navigateToRoute("game-over", {
+            msg: `${imr.player == "Player1" ? "Player2" : "Player1"} WINS!!!!`,
+          });
         }
       } else {
         this.action = "miss";
